3130ef240cc2031754f7255864d8273d0e7650e0
ARUHIZA CARLOS <aruhiza87@gmail.com>
2025-02-17 23:32:21 -0400

    variables y artimetica basica PYTHON

---
+++ aritmetica_y_variables.py
@@ -0,0 +1,53 @@
+print("Hola mundillo")
+
+print(1+2)   #suma
+
+print(9-5)  #resta
+
+print(5*4)  #multipl    
+
+print(50/2) #divison
+
+print(3**2) #elevado a..
+
+print(((1 + 3) * (9 - 2) / 2) ** 2)   # operacion con parentesis operacioens agrupadas
+
+#variables
+
+variable=5+2
+print(variable)
+
+
+mi_variable = 6
+print(mi_variable)
+
+mi_variable = 22
+print(mi_variable)
+
+print(variable)
+print(mi_variable)
+
+#creamos variables
+
+num_anios=5
+dias_por_anio=365
+horas_por_dia=24
+minutos_por_hora=60
+segundos_por_minuto=60
+
+#calculamos el total de segundos en 5 anios
+
+totalSegundos = segundos_por_minuto * minutos_por_hora * horas_por_dia * dias_por_anio * num_anios
+print(totalSegundos) 
+
+# resultado : 126144000
+
+#calculando anios bisiestos
+dias_por_anio = 365.25
+
+#calculando total de segundos en 5 anios
+
+totalSegundos = segundos_por_minuto * minutos_por_hora * horas_por_dia * dias_por_anio * num_anios
+print(totalSegundos)
+
+print(segundos_por_minuto)
